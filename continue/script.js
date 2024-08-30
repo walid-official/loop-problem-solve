df387705c229801f6e201c0850d2665947cb491a
walid hasan <walid.official8@gmail.com>
2024-08-30 14:23:40 +0600

    loop continue

---
+++ continue/script.js
@@ -0,0 +1,23 @@
+// problem-1
+//********** 
+
+
+// for(let jor = 0;jor <= 10; jor++){
+//     if(jor % 2 === 1){
+//         continue;
+//     }
+//     console.log(jor);
+// }
+
+
+// problem-2
+//*********
+
+
+for(let num = 55; num <= 85; num += 2){
+
+    if(num % 5 === 0){
+        continue;
+    }
+    console.log(num);
+}
