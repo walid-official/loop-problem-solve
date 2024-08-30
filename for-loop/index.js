523b5119009c3ad9cc32ffe5ab20a2b56fd5348a
walid hasan <walid.official8@gmail.com>
2024-08-30 12:22:34 +0600

    for loop problem is solved

---
+++ for-loop/index.js
@@ -0,0 +1,80 @@
+// for 1. problem-1
+//***************** 
+
+// for(let message = 0; message <= 60; message++){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!");
+// }
+
+
+
+// for 1. problem-2
+//***************** 
+
+
+// for(let odd = 61; odd <= 100; odd += 2){
+//     console.log(odd);
+// }
+
+
+// for(let odd = 61; odd <= 100; odd++){
+//     if(odd % 2 === 1){
+//         console.log(odd);
+//     }
+// }
+
+
+// for(let odd = 61; odd <= 100; odd++){
+//     if(odd % 2 !== 0){
+//         console.log(odd);
+//     }
+// }
+
+
+// for 1. problem-3
+//***************** 
+
+
+
+// sum = 0;
+// for(let odd = 91; odd <= 130; odd++){
+//     if(odd % 2 == 1){
+//         console.log(odd);
+//         sum += odd;
+//         console.log('this is a sum of odd numbers ' + sum);
+//     }
+// }
+// console.log(sum);
+
+// sum = 0;
+// for(let jor = 51; jor <= 85; jor++){
+//     if(jor % 2 !== 1){
+//         console.log(jor);
+//         sum += jor;
+//         console.log('this is a sum of odd numbers ' + sum);
+//     }
+// }
+// console.log(sum);
+
+
+
+
+// for 1. problem-4
+//***************** 
+
+
+// let num = Number(prompt("Enter Your Number :"));
+
+// for(let multi = 1; multi <= 10; multi++){
+//     console.log(`${num} X ${multi} = ${multi*num}`);
+// }
+
+
+
+
+// for 1. problem-5
+//***************** 
+
+
+// for(let incr = 81; incr >= 65; incr--){
+//     console.log(incr);
+// }
